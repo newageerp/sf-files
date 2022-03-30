9aaf1559e1455cfed20196e9e2c6cac4347a94b4
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-03-30 05:24:16 +0000

    files service

--- Controller/FilesController.php
+++ Controller/FilesController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 /**
  * @Route("/app/nae-core/files")
